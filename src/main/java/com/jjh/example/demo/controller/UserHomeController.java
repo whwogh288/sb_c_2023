4f2c23c5f60c69e1ca681579951c2741e55faf1c
whwogh288 <jaehojo2731@gmail.com>
2023-04-04 15:52:58 +0900

    액션메서드에서 각종 리턴타입 실험

--- src/main/java/com/jjh/example/demo/controller/UserHomeController.java
+++ src/main/java/com/jjh/example/demo/controller/UserHomeController.java
@@ -1,27 +1,95 @@
 package com.jjh.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Controller
 public class UserHomeController {
-	private int count;
-	
-	public UserHomeController() {
-		count = 0;
-	}
-	@RequestMapping("/usr/home/getCount")
+	@RequestMapping("/usr/home/getString")
 	@ResponseBody
-	public int getCount() {
-		return count++;
+	public String getString() {
+		return "HI";			
 	}
-	@RequestMapping("/usr/home/doSetCount")
+	@RequestMapping("/usr/home/getInt")
 	@ResponseBody
-	public String doSetCount(int count) {
-		this.count = count;
-		return "Count값이  "+ this.count + "으로 초기화되었습니다.";
+	public int getInt() {
+		return 5;			
+	}
+	@RequestMapping("/usr/home/getFloat")
+	@ResponseBody
+	public float getFloat() {
+		return 5.5f;			
+	}
+	@RequestMapping("/usr/home/getDouble")
+	@ResponseBody
+	public double getDouble() {
+		return 5.5;			
+	}
+	@RequestMapping("/usr/home/getBoolean")
+	@ResponseBody
+	public boolean getBoolean() {
+		return true;			
+	}
+	@RequestMapping("/usr/home/getCharacter")
+	@ResponseBody
+	public char getCharacter() {
+		return 'a';			
+	}
+	@RequestMapping("/usr/home/getMap")
+	@ResponseBody
+	public Map<String, Object> getMap() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("철수나이", 22);
+		map.put("영희나이", 21);
+		
+		return map;			
+	}
+	@RequestMapping("/usr/home/getList")
+	@ResponseBody
+	public List<String> getList() {
+		List<String> list = new ArrayList<>();
+		list.add("철수");
+		list.add("영희");
+		
+		return list;			
+	}
+	@RequestMapping("/usr/home/getArticle")
+	@ResponseBody
+	public Article getArticle() {
+		Article article = new Article(1, "제목1");
+		
+		return article;			
+	}
+	@RequestMapping("/usr/home/getArticles")
+	@ResponseBody
+	public List<Article> getArticles() {
+		Article article1 = new Article(1, "제목1");
+		Article article2 = new Article(2, "제목2");
+		
+		List<Article> list = new ArrayList<>();
+		list.add(article1);
+		list.add(article2);
+		
+		return list;			
 	}
 }
  
-   
+ @Data
+ @NoArgsConstructor
+ @AllArgsConstructor
+class Article{
+	public int id;
+	public String title;
+	
+	
+}
