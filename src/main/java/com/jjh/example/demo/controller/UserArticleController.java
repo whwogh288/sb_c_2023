a418dde8d1c7dc9d8fe0ad2656c0031e8230d6e2
whwogh288 <jaehojo2731@gmail.com>
2023-04-07 16:06:27 +0900

    게시물 수정 액션 구현

--- src/main/java/com/jjh/example/demo/controller/UserArticleController.java
+++ src/main/java/com/jjh/example/demo/controller/UserArticleController.java
@@ -58,6 +58,14 @@ public class UserArticleController {
 		
 		articles.remove(article);
 	}
+	
+	private void modifyArticle(int id, String title, String body) {
+		Article article = getArticle(id);
+		
+		article.setTitle(title);
+		article.setBody(body);
+		
+	}
 	//서비스 메서드 끝
 
 	//액션 메서드 시작
@@ -86,6 +94,20 @@ public class UserArticleController {
 		deleteArticle(id); 
 		
 		return id + "번 게시물이 삭제되었습니다.";
+	}
+	
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody
+	public String doModify(int id, String title, String body) {
+		Article article = getArticle(id);
+		
+		if( article == null) {
+			return id + "번 게시물이 존재하지 않습니다.";
+		}
+		
+		modifyArticle(id, title, body); 
+		
+		return id + "번 게시물이 수정되었습니다.";
 	}
 	//액션 메서드 끝
 }
