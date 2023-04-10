921c0736a4a8b50463b9097bead6484048097ac1
whwogh288 <jaehojo2731@gmail.com>
2023-04-10 13:48:09 +0900

    UsrArticleController의 서빗스 로직을 ArticleService로 옮김

--- src/main/java/com/jjh/example/demo/controller/UserHomeController.java
+++ src/main/java/com/jjh/example/demo/controller/UserHomeController.java
@@ -13,72 +13,10 @@ import com.jjh.example.demo.vo.Article;
 
 @Controller
 public class UserHomeController {
-	@RequestMapping("/usr/home/getString")
+	@RequestMapping("/usr/home/main")
 	@ResponseBody
-	public String getString() {
-		return "HI";			
-	}
-	@RequestMapping("/usr/home/getInt")
-	@ResponseBody
-	public int getInt() {
-		return 5;			
-	}
-	@RequestMapping("/usr/home/getFloat")
-	@ResponseBody
-	public float getFloat() {
-		return 5.5f;			
-	}
-	@RequestMapping("/usr/home/getDouble")
-	@ResponseBody
-	public double getDouble() {
-		return 5.5;			
-	}
-	@RequestMapping("/usr/home/getBoolean")
-	@ResponseBody
-	public boolean getBoolean() {
-		return true;			
-	}
-	@RequestMapping("/usr/home/getCharacter")
-	@ResponseBody
-	public char getCharacter() {
-		return 'a';			
-	}
-	@RequestMapping("/usr/home/getMap")
-	@ResponseBody
-	public Map<String, Object> getMap() {
-		Map<String, Object> map = new HashMap<>();
-		map.put("철수나이", 22);
-		map.put("영희나이", 21);
-		
-		return map;			
-	}
-	@RequestMapping("/usr/home/getList")
-	@ResponseBody
-	public List<String> getList() {
-		List<String> list = new ArrayList<>();
-		list.add("철수");
-		list.add("영희");
-		
-		return list;			
-	}
-	@RequestMapping("/usr/home/getArticle")
-	@ResponseBody
-	public Article getArticle() {
-		Article article = new Article(1, "제목1", "내용1");
-		
-		return article;			
-	}
-	@RequestMapping("/usr/home/getArticles")
-	@ResponseBody
-	public List<Article> getArticles() {
-		Article article1 = new Article(1, "제목1", "내용1");
-		Article article2 = new Article(2, "제목2", "내용2");
-		
-		List<Article> list = new ArrayList<>();
-		list.add(article1);
-		list.add(article2);
-		
-		return list;			
+	public String Main() {
+		return "Hello World";			
 	}
 }
  
