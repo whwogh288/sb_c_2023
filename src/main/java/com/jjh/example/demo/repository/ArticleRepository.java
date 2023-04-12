2b2fa47adf78ca170288c178955bba0e064ddcd1
whwogh288 <jaehojo2731@gmail.com>
2023-04-12 16:53:00 +0900

    DELETE, UPDATE, SELECT 관련 쿼리를 메서드와 연결

--- src/main/java/com/jjh/example/demo/repository/ArticleRepository.java
+++ src/main/java/com/jjh/example/demo/repository/ArticleRepository.java
@@ -2,9 +2,11 @@ package com.jjh.example.demo.repository;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.jjh.example.demo.vo.Article;
 
@@ -13,12 +15,15 @@ public interface ArticleRepository {
 	// 서비스 메서드 시작
 	public Article writeArticle(String title, String body);
 	
-	@Select("select * from article where id = #{id}")
+	@Select("SELECT * FROM article WHERE id = #{id}")
 	public Article getArticle(@Param("id") int id);
 	
-	public void deleteArticle(int id);
+	@Delete("DELETE FROM article WHERE id = #{id}")
+	public void deleteArticle(@Param("id") int id);
 	
-	public void modifyArticle(int id, String title, String body);
+	@Update("UPDATE article SET title = #{title}, `body` = #{body}, updateDate = NOW() WHERE id = #{id}")
+	public void modifyArticle(@Param("id") int id, @Param("title") String title, @Param("body") String body);
 	
+	@Select("SELECT * FROM article ORDER BY id DESC")
 	public List<Article> getArticles();
 }
