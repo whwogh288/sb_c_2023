89b99a933e1d07cd7e352c9d78380704ae379f40
whwogh288 <jaehojo2731@gmail.com>
2023-04-14 16:39:48 +0900

    회원테이블 추가

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,9 @@
+# DB생성
 DROP DATABASE IF EXISTS demo;
 CREATE DATABASE demo;
 USE demo;
 
+# 게시물 테이블 생성
 CREATE TABLE article (
     id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     regDate DATETIME NOT NULL,
@@ -10,10 +12,11 @@ CREATE TABLE article (
     `body` TEXT NOT NULL
 );
 
+# 게시물, 테스트 데이터 생성
 INSERT INTO article
 SET regDate = NOW(),
 updateDate = NOW(),
-title = '제목 1',
+title = '제목 5',
 `body` = '내용 1';
 
 INSERT INTO article
@@ -28,4 +31,53 @@ updateDate = NOW(),
 title = '제목 3',
 `body` = '내용 3';
 
-SELECT LAST_INSERT_ID();
+# 회원 테이블 생성
+CREATE TABLE `member` (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    loginId CHAR(20) NOT NULL,
+    loginPw CHAR(60) NOT NULL,
+    authLevel SMALLINT(2) UNSIGNED DEFAULT 3 COMMENT '권한레벨(3=일반, 7=관리자)',
+    `name` CHAR(20) NOT NULL,
+    nickname CHAR(20) NOT NULL,
+    cellphonNo CHAR(20) NOT NULL,
+    email CHAR(50) NOT NULL,
+    delStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '탈퇴여부(0=탈퇴전, 1=탈퇴)',
+    delDate DATETIME COMMENT '탈퇴날짜'
+);
+
+# 회원, 테스트 데이터 생성(관리자 회원)
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = 'admin',
+loginPw = 'admin',
+authLevel = 7,
+`name` = '관리자',
+nickname = '관리자',
+cellphonNo = '010-1111-2222',
+email = 'xxxx@naver.com';
+
+# 회원, 테스트 데이터 생성(일반 회원)
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = 'user1',
+loginPw = 'user1',
+`name` = '사용자1',
+nickname = '사용자1',
+cellphonNo = '010-2222-3333',
+email = 'xxxx@naver.com';
+
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = 'user2',
+loginPw = 'user2',
+`name` = '사용자2',
+nickname = '사용자2',
+cellphonNo = '010-3333-4444',
+email = 'xxxx@naver.com';
+
+SELECT * FROM `member`
