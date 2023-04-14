b82c913dae0e5a07f1b8cdf4e2d6f833dbf6a88a
whwogh288 <jaehojo2731@gmail.com>
2023-04-14 10:52:29 +0900

    게시물 수정시, 입력된 정보만 수정

--- src/main/java/com/jjh/example/demo/repository/ArticleRepository.java
+++ src/main/java/com/jjh/example/demo/repository/ArticleRepository.java
@@ -2,12 +2,8 @@ package com.jjh.example.demo.repository;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
 
 import com.jjh.example.demo.vo.Article;
 
@@ -15,13 +11,13 @@ import com.jjh.example.demo.vo.Article;
 public interface ArticleRepository {
 
 	public void writeArticle(@Param("title") String title, @Param("body") String body);
-	
+
 	public Article getArticle(@Param("id") int id);
-	
+
 	public void deleteArticle(@Param("id") int id);
-		
+
 	public void modifyArticle(@Param("id") int id, @Param("title") String title, @Param("body") String body);
-	
+
 	public List<Article> getArticles();
 
 	public int getLastInsertId();
