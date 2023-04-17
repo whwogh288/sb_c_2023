3fd87d91b3f6aa7d2c1e4a3047a4d0dc16d2e470
whwogh288 <jaehojo2731@gmail.com>
2023-04-17 16:04:04 +0900

    회원가입시, 중복 아이디 체크

--- src/main/java/com/jjh/example/demo/service/MemberService.java
+++ src/main/java/com/jjh/example/demo/service/MemberService.java
@@ -14,9 +14,19 @@ public class MemberService {
 	}
 
 	public int join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
+		Member oldMember = getMemberByLoginId(loginId);
+		
+		if (oldMember != null) {
+			return -1;
+		}
+		
 		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
 		return memberRepository.getLasteInsertId();
 	
+	}
+
+	private Member getMemberByLoginId(String loginId) {
+		return memberRepository.getMemberByLoginId(loginId);
 	}
 
 	public Member getMemberById(int id) {
