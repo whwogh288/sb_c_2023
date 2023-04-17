e49656b2544ec9511993109f78dd6ef678da86c4
whwogh288 <jaehojo2731@gmail.com>
2023-04-17 11:24:50 +0900

    MemberRepository 추가 후 , 회원가입 처리

--- src/main/java/com/jjh/example/demo/service/MemberService.java
+++ src/main/java/com/jjh/example/demo/service/MemberService.java
@@ -2,12 +2,18 @@ package com.jjh.example.demo.service;
 
 import org.springframework.stereotype.Service;
 
+import com.jjh.example.demo.repository.MemberRepository;
+
 @Service
 public class MemberService {
-	//private MemberRepository memberRepository;
+	private MemberRepository memberRepository;
+	
+	public MemberService(MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
+	}
 
 	public void join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
-		//memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
 	
 	}
 }
