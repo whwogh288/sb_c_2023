03b6dd2379ff7743a831c5afa45ec3b2ace2b7c4
whwogh288 <jaehojo2731@gmail.com>
2023-04-19 13:11:43 +0900

    범용 보고서 클래스, ResultData 구현

---
+++ src/main/java/com/jjh/example/demo/vo/ResultData.java
@@ -0,0 +1,35 @@
+package com.jjh.example.demo.vo;
+
+import lombok.Getter;
+
+public class ResultData {
+	// S-1 
+	// F-1
+	@Getter
+	private String resultCode;
+	@Getter
+	private String msg;
+	@Getter
+	private Object data1;
+	
+	private ResultData() {
+		
+	}
+	
+	public static ResultData from(String resultCode, String msg, Object data1) {
+		ResultData rd = new ResultData();
+		rd.resultCode = resultCode;
+		rd.msg = msg;
+		rd.data1 = data1;
+		
+		return rd;
+	}
+
+	public boolean isSucccess() {
+		return resultCode.startsWith("S-");	
+	}
+	
+	public boolean isFail() {
+		return isFail() == false;
+	}
+}
