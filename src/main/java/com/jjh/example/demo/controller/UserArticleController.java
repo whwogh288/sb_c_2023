00d5d10f0193460e7f06b7ee0598d3e3811426a7
whwogh288 <jaehojo2731@gmail.com>
2023-04-19 15:32:39 +0900

    /usr/article/doAdd에 ResultData 적용

--- src/main/java/com/jjh/example/demo/controller/UserArticleController.java
+++ src/main/java/com/jjh/example/demo/controller/UserArticleController.java
@@ -20,12 +20,21 @@ public class UserArticleController {
 	// 액션 메서드 시작
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
-	public Article doAdd(String title, String body) {
-		int id = articleService.writeArticle(title, body);
+	public ResultData doAdd(String title, String body) {
+		if ( Ut.empty(title)) {
+			return ResultData.from("F-1", "제목을 입력해주세요.");
+		}
+		
+		if ( Ut.empty(body)) {
+			return ResultData.from("F-2", "내용을 입력해주세요.");
+		}
+		
+		ResultData writeArticleRd = articleService.writeArticle(title, body);
+		int id = (int)writeArticleRd.getData1();
 
 		Article article = articleService.getArticle(id);
 		
-		return article;
+		return ResultData.from(writeArticleRd.getResultCode(), writeArticleRd.getMsg(), article);
 	}
 
 	@RequestMapping("/usr/article/getArticles")
@@ -41,7 +50,7 @@ public class UserArticleController {
 		Article article = articleService.getArticle(id);
 
 		if (article == null) {
-			return ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다.", id));
+			return ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다.", id), null);
 		}
 
 		return ResultData.from("S-1", Ut.f("%d번 게시물입니다.", id), article);
--- src/main/java/com/jjh/example/demo/service/ArticleService.java
+++ src/main/java/com/jjh/example/demo/service/ArticleService.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.jjh.example.demo.repository.ArticleRepository;
+import com.jjh.example.demo.utill.Ut;
 import com.jjh.example.demo.vo.Article;
+import com.jjh.example.demo.vo.ResultData;
 
 @Service
 public class ArticleService {
@@ -15,9 +17,11 @@ public class ArticleService {
 		this.articleRepository = articleRepository;
 	}
 	
-	public int writeArticle(String title, String body) {
+	public ResultData writeArticle(String title, String body) {
 		articleRepository.writeArticle(title, body);
-		return articleRepository.getLastInsertId();
+		int id = articleRepository.getLastInsertId();
+		
+		return ResultData.from("S-1", Ut.f("%d번 게시물이 생성되었습니다.", id), id);
 	}
 
 	public List<Article> getArticles() {
