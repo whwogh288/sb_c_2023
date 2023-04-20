b4b6a7c309f120ccc5ad879ddc088e233835f9da
whwogh288 <jaehojo2731@gmail.com>
2023-04-20 15:18:25 +0900

    게시물 테이블에 회원번호 칼럼 추가

--- db/schema.sql
+++ db/schema.sql
@@ -34,8 +34,6 @@ title = '제목 3',
 # 회원 테이블 생성
 CREATE TABLE `member` (
     id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    regDate DATETIME NOT NULL,
-    updateDate DATETIME NOT NULL,
     loginId CHAR(20) NOT NULL,
     loginPw CHAR(60) NOT NULL,
     authLevel SMALLINT(2) UNSIGNED DEFAULT 3 COMMENT '권한레벨(3=일반, 7=관리자)',
@@ -43,41 +41,50 @@ CREATE TABLE `member` (
     nickname CHAR(20) NOT NULL,
     cellphoneNo CHAR(20) NOT NULL,
     email CHAR(50) NOT NULL,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
     delStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '탈퇴여부(0=탈퇴전, 1=탈퇴)',
     delDate DATETIME COMMENT '탈퇴날짜'
 );
 
 # 회원, 테스트 데이터 생성(관리자 회원)
 INSERT INTO `member`
-SET regDate = NOW(),
-updateDate = NOW(),
-loginId = 'admin',
+SET loginId = 'admin',
 loginPw = 'admin',
 authLevel = 7,
 `name` = '관리자',
 nickname = '관리자',
 cellphoneNo = '010-1111-2222',
-email = 'xxxx@naver.com';
+email = 'xxxx@naver.com',
+regDate = NOW(),
+updateDate = NOW();
 
 # 회원, 테스트 데이터 생성(일반 회원)
 INSERT INTO `member`
-SET regDate = NOW(),
-updateDate = NOW(),
-loginId = 'user1',
+SET loginId = 'user1',
 loginPw = 'user1',
 `name` = '사용자1',
 nickname = '사용자1',
 cellphoneNo = '010-2222-3333',
-email = 'xxxx@naver.com';
+email = 'xxxx@naver.com',
+regDate = NOW(),
+updateDate = NOW();
 
 INSERT INTO `member`
-SET regDate = NOW(),
-updateDate = NOW(),
-loginId = 'user2',
+SET loginId = 'user2',
 loginPw = 'user2',
 `name` = '사용자2',
 nickname = '사용자2',
 cellphoneNo = '010-3333-4444',
-email = 'xxxx@naver.com';
+email = 'xxxx@naver.com',
+regDate = NOW(),
+updateDate = NOW();
 
+#게시물 테이블에 회원정보 추가
+ALTER TABLE article ADD COLUMN memberId INT(10) UNSIGNED NOT NULL AFTER `updateDate`;
 SELECT * FROM `member`;
+
+#기존 게시물의 작성자를 2번호로 지정
+UPDATE article
+SET memberId = 2
+WHERE memberId = 0;
