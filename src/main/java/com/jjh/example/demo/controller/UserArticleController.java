f60c6b160766ca351c0b3e71626d2ef256e068e0
whwogh288 <jaehojo2731@gmail.com>
2023-04-28 11:04:16 +0900

    게시물 삭제 후 replace로 이동

--- src/main/java/com/jjh/example/demo/controller/UserArticleController.java
+++ src/main/java/com/jjh/example/demo/controller/UserArticleController.java
@@ -108,7 +108,7 @@ public class UserArticleController {
 
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
-	public ResultData doDelete(HttpSession httpSession, int id) {
+	public String doDelete(HttpSession httpSession, int id) {
 		boolean isLogined = false;
 		int loginedMemberId = 0;
 		
@@ -118,22 +118,22 @@ public class UserArticleController {
 		}
 		
 		if(isLogined == false) {
-			return ResultData.from("F-A", "로그인 후 이용해주세요.");
+			return Ut.jsHistoryBack("로그인 후 이용해주세요.");
 		}
 		
 		Article article = articleService.getForPrintArticle(loginedMemberId, id);
 
 		if(article.getMemberId() != loginedMemberId) {
-			return ResultData.from("F-2", "권한이 없습니다.");
+			return Ut.jsHistoryBack("권한이 없습니다.");
 		}
 		
 		if (article == null) {
-			return ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지않습니다.", id));
+			return Ut.jsHistoryBack(Ut.f("%d번 게시물이 존재하지않습니다.", id));
 		}
 
 		articleService.deleteArticle(id);
 
-		return ResultData.from("S-1", Ut.f("%d번 게시물을 삭제하였습니다.", id), "id", id);
+		return Ut.jsReplace(Ut.f("%d번 게시물을 삭제하였습니다.", id), "../article/list");
 	}
 
 	@RequestMapping("/usr/article/doModify")
